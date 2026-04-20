11d7b0c24463d23a49d70655804a97a3c5d5a0f4
kotorin123 <piyo_0204_8953@yahoo.co.jp>
2026-04-20 21:32:23 +0900

    placeable?が真偽値を返すように修正し、finished?の誤判定を解消

--- lib/reversi_methods.rb
+++ lib/reversi_methods.rb
@@ -78,14 +78,16 @@ module ReversiMethods
   end
 
   def placeable?(board, attack_stone_color)
+    placeable = false
     board.each_with_index do |cols, row|
       cols.each_with_index do |cell, col|
         next unless cell == BLANK_CELL
 
         position = Position.new(row, col)
-        return true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
+        placeable = true if put_stone(board, position.to_cell_ref, attack_stone_color, dry_run: true)
       end
     end
+    placeable
   end
 
   def count_stone(board, stone_color)
